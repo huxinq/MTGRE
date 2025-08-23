9a52258f3c7da2a63e34a4e17116c4fe51770cc2
Xinqiao Hu <xhuat@connect.ust.hk>
2025-08-23 09:57:35 +0800

    added span_id and cause_seq to TraceEntry

--- MTGRE/src/logging/TraceTypes.jl
+++ MTGRE/src/logging/TraceTypes.jl
@@ -1,12 +1,14 @@
 struct TraceEntry
-    seq   ::Int
-    phase ::Symbol
-    kind  ::Symbol
-    t     ::Int
-    cohort::Int
-    actor ::Union{Int,Nothing}
-    tag   ::Symbol
-    data  ::NamedTuple
+    seq      ::Int            # total order
+    phase    ::Symbol         # :microcycle | :priority
+    kind     ::Symbol         # :cohort | :commit | :ep | :stack | :frame | :asba | :checkpoint | :event
+    t        ::Int
+    cohort   ::Int
+    actor    ::Union{Int,Nothing}
+    tag      ::Symbol
+    span_id  ::Int            # 0 if none; same id for frame enter/exit, same microcycle, etc.
+    cause_seq::Int            # 0 if none; seq of the entry that scheduled/caused this one
+    data     ::NamedTuple
 end
 
 mutable struct Trace
