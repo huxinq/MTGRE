8d52fb3203ecb201a6bccdca4c58dcceffeda816
Xinqiao Hu <xhuat@connect.ust.hk>
2025-08-24 18:59:47 +0800

    have schedule! return nothing

--- src/engine/schedule.jl
+++ src/engine/schedule.jl
@@ -1,8 +1,8 @@
-using ..MTGRE.Core: GameState, Op, VTime, getvtime, Cohort
 export schedule!
 
-function schedule!(S::GameState, ops::Vector{Op}; t::VTime=getvtime(S), cohort::Cohort=0)
+function schedule!(S::GameState, ops::AbstractVector{<:Op}; t::VTime=getvtime(S), cohort::Cohort=Cohort(0))
     q = get!(S.sched, t) do; Vector{Batch}(); end
     push!(q, Batch(cohort, ops))
+    return nothing
 end
 schedule!(S::GameState, ops::Op...; kwargs...) = schedule!(S, collect(ops); kwargs...)
